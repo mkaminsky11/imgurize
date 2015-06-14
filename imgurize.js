8d2423e72e61d6a3163f3e9dfed4893c8d922f18
Michael Kaminsky <mkaminsky11@gmail.com>
2015-06-14 10:22:57 -0400

    basic layout, get gallery

--- imgurize.js
+++ imgurize.js
@@ -2,17 +2,19 @@
 //74542f2b794fdb874d9d790de40dccf7bb09a8da
 var blessed = require('blessed');
 var fs = require('fs');
-var pictureTube = require('picture-tube')
-var tube = pictureTube();
-tube.pipe(process.stdout);
+var ai = require('ascii-images');
+var request = require('request');
 
-//imgurize.width = process.stdout.getWindowSize()[0];
-//imgurize.height = process.stdout.getWindowSize()[1];
 var screen = blessed.screen({
   autoPadding: true,
   smartCSR: true
 });
 screen.title = "imgurizer";
+
+var layout = blessed.layout({
+  width: '100%',
+  height: '100%'
+});
 
 var title_text = blessed.text({
   width: '100%',
@@ -40,20 +42,62 @@ var content_box = blessed.box({
     border: {
       fg: 'white'
     }
+  },
+  scrollable: true
+});
+
+var post_title = blessed.text({
+  width: '100%',
+  align: 'center',
+  content: 'welcome to imgurizer!',
+  style: {
+    bold: true,
+    fg: 'white',
+    //underline: 'white'
+  },
+});
+
+var title_line = blessed.line({
+  width: '100%',
+  orientation: 'horizontal',
+  style: {
+    fg: 'white'
   }
 });
 
-screen.append(content_box);
-screen.append(title_text);
+screen.append(layout);
+layout.append(title_text);
+layout.append(content_box);
+content_box.append(post_title);
+//content_box.append(title_line);
+
+getGallery(0, function(err, res, body){
+  //TODO: handle error
+  gallery = JSON.parse(body).data;
+  setPost(0);
+});
 
 screen.render();
 
-//fs.createReadStream('img.png').pipe(tube);
+//ai('img.png', function(result){
+//  console.log(result);
+//});
 
-var imgurize = {
+var num = 0;
+var gallery = null;
+var page = 0;
 
+function getGallery(page, callback){
+  request({
+    url: "https://api.imgur.com/3/gallery.json?page=" + page + "&perPage=30",
+    headers: {
+      "Authorization": "Client-ID 0b4d9e43c145774"
+    }
+  }, callback);
 };
 
-function getGallery(){
-  //https://api.imgur.com/3/gallery.json
-};
+function setPost(num){
+  var obj = gallery[num];
+  //console.log(obj);
+  post_title.setContent(obj.title);
+}
---
+++ node_modules/ascii-images/tests/ascii-images-tests.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var assert = require('assert');
+//var ascii-images = require('../');
+
+// test 1
+assert(true);
